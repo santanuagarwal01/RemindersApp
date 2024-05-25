a90782019d4227d386f29c4d6d3b6dc3d6212df8
Santanu Agarwal <santanuagarwal01@gmail.com>
2024-05-25 13:42:39 +0530

    Added notifications for reminders: final build

---
+++ RemindersApp/Managers/NotificationManager.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationManager.swift
+//  RemindersApp
+//
+//  Created by Santanu Agarwal on 25/05/24.
+//
+
+import Foundation
---
+++ RemindersApp/Views/ReminderStatsView.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderStatsView.swift
+//  RemindersApp
+//
+//  Created by Santanu Agarwal on 24/05/24.
+//
+
+import SwiftUI
+
+struct ReminderStatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderStatsView()
+}
